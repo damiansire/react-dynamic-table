aa958970b4ba71a755e47dfc6b5bb1bd66711363
Damian Sire <damiansire@hotmail.com>
2023-09-12 05:30:57 -0300

    refactor: add new structure

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,22 +1,70 @@
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import Button from "./Button";
+import { ButtonProps } from "./Button.types";
 
-// You can learn about this: https://storybook.js.org/docs/react/writing-stories/introduction
-
-export default {
-  title: "Button",
+const meta: Meta<typeof Button> = {
   component: Button,
-} as ComponentMeta<typeof Button>;
+  title: "Marbella/Button",
+  argTypes: {},
+};
+export default meta;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+type Story = StoryObj<typeof Button>;
 
-export const Primary = Template.bind({});
+export const Primary: Story = (args: ButtonProps) => (
+  <Button data-testId="InputField-id" {...args} />
+);
 Primary.args = {
-  label: "Primary",
+  primary: true,
+  disabled: false,
+  text: "Primary",
 };
 
-export const Secondary = Template.bind({});
+export const Secondary: Story = (args: ButtonProps) => (
+  <Button data-testId="InputField-id" {...args} />
+);
 Secondary.args = {
-  label: "Secondary",
+  primary: false,
+  disabled: false,
+  text: "Secondary",
+};
+
+export const Disabled: Story = (args: ButtonProps) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Disabled.args = {
+  primary: false,
+  disabled: true,
+  text: "Disabled",
+};
+
+export const Small: Story = (args: ButtonProps) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Small.args = {
+  primary: true,
+  disabled: false,
+  size: "small",
+  text: "Small",
+};
+
+export const Medium: Story = (args: ButtonProps) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Medium.args = {
+  primary: true,
+  disabled: false,
+  size: "medium",
+  text: "Medium",
+};
+
+export const Large: Story = (args: ButtonProps) => (
+  <Button data-testId="InputField-id" {...args} />
+);
+Large.args = {
+  primary: true,
+  disabled: false,
+  size: "large",
+  text: "Large",
 };
